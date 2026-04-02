42c601d35b982b431295faa3ba9b2e02a0df4e2f
Matheus Quirino <contato.matheuspquirino@gmail.com>
2026-04-02 20:55:09 -0300

    SCA-8-criacao-main-para-javafx (#11)
    * [SCA-8]feat: Criação da tela prinicpal do JavaFX #in-progress
    
    * [SCA-8]feat: Adicionando Main.java #in-progress
    
    ---------
    
    Co-authored-by: Matheus Quirino <matheus.quirino4@fatec.sp.gov.br>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // O "/" inicial indica que a busca começa na raiz da pasta resources
+        URL fxmlLocation = getClass().getResource("/view/teste2.fxml");
+
+        if (fxmlLocation == null) {
+            throw new RuntimeException("Arquivo FXML não encontrado em: /view/teste2.fxml");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxmlLocation);
+        Parent root = loader.load();
+
+        // Configuração da Janela (Cena)
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("DataTech API - 2º Semestre");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
